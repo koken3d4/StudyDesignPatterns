cc4ccf12df4c957277014b0f7f605bb10d82249f
koken3d4 <koken3d4@gmail.com>
2020-09-19 19:23:41 +0900

    master

--- StudyDesignPatterns/AbstractFactory.cs
+++ StudyDesignPatterns/AbstractFactory.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace StudyDesignPatterns
 {
-    class AbstractFactory
+    /// <summary>
+    /// 8CHの内容
+    /// </summary>
+    public abstract class Item
     {
+        protected string caption;
+        public Item(string caption)
+        { this.caption = caption; }
+
+        public abstract String makeHTML();
+    }
+
+    public abstract class Link : Item
+    {
+        protected string url;
+        public Link(string caption,string url)
+        {
+            super(caption);
+            this.url = url;
+        }
+    
+    
     }
 }
+
